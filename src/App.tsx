e4d6066956a3bb5cf77a9471ab8fd433a9f8f645
ranjith8888 <goudp1234@gmail.com>
2025-02-19 11:19:34 +0530

    Fixed homepage field for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // App.tsx (Main Component)
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box, CssBaseline } from '@mui/material';
 import Counter from './components/Counter.tsx';
 import UserForm from './components/UserForm.tsx';
@@ -7,7 +7,7 @@ import Editor from './components/Editor.tsx';
 
 function App() {
   return (
-      <Router >
+    <BrowserRouter basename="/assessmentapp">
       <CssBaseline />
       <Box sx={{ padding: 4 }}>
         <Routes>
@@ -20,7 +20,7 @@ function App() {
           } />
         </Routes>
       </Box>
-    </Router>
+    </BrowserRouter>
   );
 }
 
